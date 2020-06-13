b2995a2e204a91f7238370038626fd28e0726e0a
unknown <975569220@22.com>
2020-06-14 00:38:58 +0800

    添加随机功能

--- src/components/RandomNum.vue
+++ src/components/RandomNum.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <button @click="add">添加元素</button>
+    <button @click="random">打乱顺序</button>
     <hr>
     <transition-group>
       <span v-for="item in numlist" :key="item">{{item}}</span>
@@ -19,6 +20,10 @@ export default {
   methods: {
     add () {
       this.numlist.splice(this.getLocate(), 0, this.numlist.length + 1)
+    },
+    random () {
+      this.numlist.sort(() => Math.random() - 0.5)
+      //另一种方法：this.numlist.sort(() => Math.random() < 0.5 ? 1 : -1)
     },
     getLocate () {
       return Math.floor(Math.random() * this.numlist.length)
